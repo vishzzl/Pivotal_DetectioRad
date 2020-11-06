6b2a2ac639c031874fd774714969695a64a406f6
Vidhika Jain <vidhika112@gmail.com>
2020-11-06 13:51:29 +0530

    latest change
    i have made one minor change please check

--- pivotal/load.py
+++ pivotal/load.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os
 from tkinter import messagebox
+import mlf
 
 
 
@@ -12,6 +13,7 @@ conn.commit()
 
 def delete_img():
     os.remove(addr)
+    mlf.root_ml.destroy()
 
 def loadBLOB(patient_id):
     global addr
