61241a9d59317de68377a7454636abd62d91135e
Vidhika Jain <vidhika112@gmail.com>
2020-12-14 10:29:13 +0530

    screen layout

--- pivotal/pivotal_form.py
+++ pivotal/pivotal_form.py
@@ -13,24 +13,7 @@ import mlf
 
 
 
-conn = sqlite3.connect("patients_pivotal_record.db")
 
-s1=conn.cursor()
-
-s1.execute('''CREATE TABLE IF NOT EXISTS patients_records_pivotal(
-                patient_id text(255)  NOT NULL,
-                patient_name text(255) NOT NULL,
-                patient_age integer NOT NULL,
-                patient_gender string NOT NULL,
-                patient_history text,
-                old_xray blob,
-                new_xray blob,
-                feedback text,
-                PRIMARY KEY(patient_id)
-                )''')
-
-
-conn.commit()
 
 global form_page
 
@@ -73,6 +56,24 @@ def back_command4():
     form_page()
 
 def update_form():
+    conn = sqlite3.connect("patients_pivotal_record.db")
+
+    s1=conn.cursor()
+
+    s1.execute('''CREATE TABLE IF NOT EXISTS patients_records_pivotal(
+                patient_id text(255)  NOT NULL,
+                patient_name text(255) NOT NULL,
+                patient_age integer NOT NULL,
+                patient_gender string NOT NULL,
+                patient_history text,
+                old_xray blob,
+                new_xray blob,
+                feedback text,
+                PRIMARY KEY(patient_id)
+                )''')
+
+
+    conn.commit()
 
     global p_id
 
@@ -141,7 +142,11 @@ def new_patients():
     global new_patients_screen
     new_patients_screen = Tk()
 
-    new_patients_screen.geometry("1208x681+180+100")
+    width_value=new_patients_screen.winfo_screenwidth()
+    height_value=new_patients_screen.winfo_screenheight()
+    new_patients_screen.geometry("%dx%d+0+0"%(width_value,height_value))
+    
+    
     #form_screen.minsize(w_x,w_y)
     #register_screen.maxsize(1924, 1055)
     new_patients_screen.resizable(0,0)
@@ -193,7 +198,7 @@ def new_patients():
     Canvas1.configure(selectforeground="white")
    
     Label1 = Label(Canvas1)
-    Label1.place(relx=0.122, rely=0.063, height=36, width=750)
+    Label1.place(relx=0.122, rely=0.063, height=36, width=1300)
     Label1.configure(activebackground="#000071")
     Label1.configure(activeforeground="white")
     Label1.configure(activeforeground="#f0f0f0f0f0f0")
@@ -208,7 +213,7 @@ def new_patients():
     Label1.configure(text='''Fill Customer Details''')
 
     label_id = Label(Canvas1)
-    label_id.place(relx=0.194, rely=0.185, height=36, width=192)
+    label_id.place(relx=0.194, rely=0.185, height=40, width=192)
     label_id.configure(background="#c0c0c0")
     label_id.configure(disabledforeground="#a3a3a3")
     label_id.configure(font=font9)
@@ -216,7 +221,7 @@ def new_patients():
     label_id.configure(text='''Patient Id''')
    
     idEntry = Entry(Canvas1)
-    idEntry.place(relx=0.388, rely=0.185,height=34, relwidth=0.402)
+    idEntry.place(relx=0.388, rely=0.185,height=40, relwidth=0.402)
     idEntry.configure(background="white")
     idEntry.configure(disabledforeground="#a3a3a3")
     idEntry.configure(font="TkFixedFont")
@@ -226,7 +231,7 @@ def new_patients():
     idEntry.configure(textvariable=c_id_entry)
    
     label_name = Label(Canvas1)
-    label_name.place(relx=0.194, rely=0.277, height=36, width=188)
+    label_name.place(relx=0.194, rely=0.277, height=40, width=188)
     label_name.configure(activebackground="#f9f9f9")
     label_name.configure(activeforeground="black")
     label_name.configure(background="#c0c0c0")
@@ -238,7 +243,7 @@ def new_patients():
     label_name.configure(text='''Name''')
   
     nameEntry = Entry(Canvas1)
-    nameEntry.place(relx=0.388, rely=0.277,height=34, relwidth=0.402)
+    nameEntry.place(relx=0.388, rely=0.277,height=40, relwidth=0.402)
     nameEntry.configure(background="white")
     nameEntry.configure(disabledforeground="#a3a3a3")
     nameEntry.configure(font="TkFixedFont")
@@ -252,7 +257,7 @@ def new_patients():
     nameEntry.configure(textvariable=c_name_entry)
   
     label_age = Label(Canvas1)
-    label_age.place(relx=0.194, rely=0.369, height=36, width=188)
+    label_age.place(relx=0.194, rely=0.369, height=40, width=188)
     label_age.configure(activebackground="#f9f9f9")
     label_age.configure(activeforeground="black")
     label_age.configure(background="#c0c0c0")
@@ -264,7 +269,7 @@ def new_patients():
     label_age.configure(text='''Age''')
   
     ageEntry = Entry(Canvas1)
-    ageEntry.place(relx=0.388, rely=0.369,height=34, relwidth=0.402)
+    ageEntry.place(relx=0.388, rely=0.369,height=40, relwidth=0.402)
     ageEntry.configure(background="white")
     ageEntry.configure(disabledforeground="#a3a3a3")
     ageEntry.configure(font="TkFixedFont")
@@ -278,7 +283,7 @@ def new_patients():
     ageEntry.configure(textvariable=c_age_entry)
   
     label_gender =Label(Canvas1)
-    label_gender.place(relx=0.194, rely=0.461, height=36, width=188)
+    label_gender.place(relx=0.194, rely=0.461, height=40, width=188)
     label_gender.configure(activebackground="#f9f9f9")
     label_gender.configure(activeforeground="black")
     label_gender.configure(background="#c0c0c0")
@@ -290,7 +295,7 @@ def new_patients():
     label_gender.configure(text='''Gender''')
   
     genderEntry = Entry(Canvas1)
-    genderEntry.place(relx=0.388, rely=0.461, height=34, relwidth=0.402)
+    genderEntry.place(relx=0.388, rely=0.461, height=40, relwidth=0.402)
   
     genderEntry.configure(background="white")
     genderEntry.configure(disabledforeground="#a3a3a3")
@@ -305,7 +310,7 @@ def new_patients():
     genderEntry.configure(textvariable=c_gender_entry)
   
     label_history = Label(Canvas1)
-    label_history.place(relx=0.194, rely=0.572, height=36, width=188)
+    label_history.place(relx=0.194, rely=0.572, height=40, width=188)
     label_history.configure(activebackground="#f9f9f9")
     label_history.configure(activeforeground="black")
     label_history.configure(background="#c0c0c0")
@@ -317,7 +322,7 @@ def new_patients():
     label_history.configure(text='''Medical History''')
   
     history_Entry = Text(Canvas1)
-    history_Entry.place(relx=0.388, rely=0.554, height=184, relwidth=0.402)
+    history_Entry.place(relx=0.388, rely=0.554, height=200, relwidth=0.410)
     history_Entry.configure(background="white")
     history_Entry.configure(font="TkFixedFont")
     history_Entry.configure(foreground="#000000")
@@ -329,7 +334,7 @@ def new_patients():
     
   
     label_xray = Label(Canvas1)
-    label_xray.place(relx=0.184, rely=0.904, height=36, width=188)
+    label_xray.place(relx=0.184, rely=0.904, height=40, width=188)
     label_xray.configure(activebackground="#f9f9f9")
     label_xray.configure(activeforeground="black")
     label_xray.configure(background="#c0c0c0")
@@ -341,7 +346,7 @@ def new_patients():
     label_xray.configure(text='''X-ray''')
   
     img = Button(Canvas1)
-    img.place(relx=0.388, rely=0.923, height=33, width=136)
+    img.place(relx=0.388, rely=0.923, height=40, width=145)
     img.configure(activebackground="#ececec")
     img.configure(activeforeground="#000000")
     img.configure(background="#141414")
@@ -355,7 +360,7 @@ def new_patients():
     img.configure(text='''select image''')
 
     submit = Button(Canvas1)
-    submit.place(relx=0.602, rely=0.923, height=33, width=166)
+    submit.place(relx=0.602, rely=0.923, height=40, width=172)
     submit.configure(activebackground="#ececec")
     submit.configure(activeforeground="#000000")
     submit.configure(background="#0f0f0f")
@@ -371,7 +376,7 @@ def new_patients():
     submit.configure(text='''Submit''')
 
     Label21= Label(Canvas1)
-    Label21.place(relx=0.524, rely=0.923, height=26, width=75)
+    Label21.place(relx=0.524, rely=0.923, height=32, width=95)
     Label21.configure(background="#d9d9d9")
     Label21.configure(disabledforeground="#a3a3a3")
     Label21.configure(foreground="#000000")
@@ -386,7 +391,7 @@ def new_patients():
     
 
     Button2 = Button(new_patients_screen)
-    Button2.place(relx=0.0, rely=0.0, height=33, width=66)
+    Button2.place(relx=0.0, rely=0.0, height=40, width=66)
     Button2.configure(activebackground="#ececec")
     Button2.configure(activeforeground="#000000")
     Button2.configure(background="#c0c0c0")
@@ -406,7 +411,9 @@ def view_record():
     global viewlog
     viewlog = Tk()
 
-    viewlog.geometry("1208x681+180+100")
+    width_value=viewlog.winfo_screenwidth()
+    height_value=viewlog.winfo_screenheight()
+    viewlog.geometry("%dx%d+0+0"%(width_value,height_value))
     #form_screen.minsize(w_x,w_y)
     #register_screen.maxsize(1924, 1055)
     viewlog.resizable(0,0)
@@ -450,7 +457,7 @@ def view_record():
     Canvas1.configure(selectforeground="white")
    
     Label1 = Label(Canvas1)
-    Label1.place(relx=0.122, rely=0.063, height=36, width=750)
+    Label1.place(relx=0.122, rely=0.063, height=36, width=1300)
     Label1.configure(activebackground="#000071")
     Label1.configure(activeforeground="white")
     Label1.configure(activeforeground="#f0f0f0f0f0f0")
@@ -465,7 +472,7 @@ def view_record():
     Label1.configure(text='''Fill Customer Details''')
 
     registration = Label(Canvas1)
-    registration.place(relx=0.209, rely=0.24, height=37, width=162)    
+    registration.place(relx=0.209, rely=0.24, height=40, width=162)    
     registration.configure(background="#c0c0c0")
     registration.configure(disabledforeground="#a3a3a3")
     registration.configure(font=font9)
@@ -473,7 +480,7 @@ def view_record():
     registration.configure(text='''registration no.''')
    
     Entry1_reg = Entry(Canvas1)
-    Entry1_reg.place(relx=0.428, rely=0.24,height=34, relwidth=0.317)
+    Entry1_reg.place(relx=0.428, rely=0.24,height=40, relwidth=0.317)
     Entry1_reg.configure(background="white")
     Entry1_reg.configure(disabledforeground="#a3a3a3")
     Entry1_reg.configure(font="TkFixedFont")
@@ -482,7 +489,7 @@ def view_record():
     Entry1_reg.configure(textvariable=get_reg)
    
     name_box = Label(Canvas1)
-    name_box.place(relx=0.209, rely=0.412, height=36, width=162)
+    name_box.place(relx=0.209, rely=0.412, height=40, width=162)
     name_box.configure(background="#c0c0c0")
     name_box.configure(disabledforeground="#a3a3a3")
     name_box.configure(font=font9)
@@ -491,7 +498,7 @@ def view_record():
     
     
     name_ebox = Entry(Canvas1)
-    name_ebox.place(relx=0.428, rely=0.412,height=34, relwidth=0.317)
+    name_ebox.place(relx=0.428, rely=0.412,height=40, relwidth=0.317)
     name_ebox.configure(background="white")
     name_ebox.configure(disabledforeground="#a3a3a3")
     name_ebox.configure(font="TkFixedFont")
@@ -504,7 +511,7 @@ def view_record():
     name_ebox.configure(textvariable=get_ename)
   
     Button1 = Button(Canvas1,command=mlf.start_ml)
-    Button1.place(relx=0.371, rely=0.635, height=33, width=206)
+    Button1.place(relx=0.385, rely=0.635, height=45, width=306)
     Button1.configure(activebackground="#ececec")
     Button1.configure(activeforeground="#000000")
     Button1.configure(background="#092748")
@@ -521,7 +528,7 @@ def view_record():
 
     
     Button2 = Button(viewlog)
-    Button2.place(relx=0.0, rely=0.0, height=33, width=66)
+    Button2.place(relx=0.0, rely=0.0, height=40, width=66)
     Button2.configure(activebackground="#ececec")
     Button2.configure(activeforeground="#000000")
     Button2.configure(background="#c0c0c0")
@@ -562,7 +569,9 @@ def form_page():
     
     
     
-    form_screen.geometry("1208x681+180+100")
+    width_value=form_screen.winfo_screenwidth()
+    height_value=form_screen.winfo_screenheight()
+    form_screen.geometry("%dx%d+0+0"%(width_value,height_value))
     #form_screen.minsize(w_x,w_y)
     #register_screen.maxsize(1924, 1055)
     form_screen.resizable(0,0)
@@ -595,7 +604,7 @@ def form_page():
     Canvas1.configure(selectforeground="white")
    
     Label1 = Label(Canvas1)
-    Label1.place(relx=0.122, rely=0.063, height=36, width=750)
+    Label1.place(relx=0.122, rely=0.063, height=36, width=1300)
     Label1.configure(activebackground="#000071")
     Label1.configure(activeforeground="white")
     Label1.configure(activeforeground="#f0f0f0f0f0f0")
@@ -612,7 +621,7 @@ def form_page():
     
 
     n_patients = Button(Canvas1)
-    n_patients.place(relx=0.316, rely=0.314, height=53, width=376)
+    n_patients.place(relx=0.360, rely=0.314, height=65, width=400)
     n_patients.configure(activebackground="#ececec")
     n_patients.configure(activeforeground="#000000")
     n_patients.configure(background="#092748")
@@ -629,7 +638,7 @@ def form_page():
     n_patients.configure(text='''Add new patient''')
    
     o_patients_1 = Button(Canvas1)
-    o_patients_1.place(relx=0.316, rely=0.517, height=53, width=376)
+    o_patients_1.place(relx=0.360, rely=0.517, height=65, width=400)
     o_patients_1.configure(activebackground="#ececec")
     o_patients_1.configure(activeforeground="#000000")
     o_patients_1.configure(background="#092748")
@@ -646,21 +655,21 @@ def form_page():
     o_patients_1.configure(text='''Existing patient''')
 
     Label3 = Label(Canvas1, text=doctorname)
-    Label3.place(relx=0.041, rely=0.207, height=26, width=142)
+    Label3.place(relx=0.041, rely=0.207, height=50, width=150)
     Label3.configure(background="#c0c0c0")
     #Label3.configure(disabledforeground="#a3a3a3")
     Label3.configure(foreground="#000000")
     
    
     Label2 = Label(Canvas1, text=doctorid)
-    Label2.place(relx=0.336, rely=0.207, height=26, width=111)
+    Label2.place(relx=0.336, rely=0.207, height=50, width=150)
     Label2.configure(background="#c0c0c0")
     #Label2.configure(disabledforeground="#a3a3a3")
     Label2.configure(foreground="#000000")
     
    
     Button1 = Button(Canvas1)
-    Button1.place(relx=0.783, rely=0.207, height=33, width=90)
+    Button1.place(relx=0.783, rely=0.207, height=40, width=95)
     Button1.configure(activebackground="#ececec")
     Button1.configure(activeforeground="#000000")
     #Button1.configure(background="#c0c0c0")
